71a9ce31110bafc2aa7d8c1f1bca1d25050c3b09
Wing5671 <91343003+Wing5671@users.noreply.github.com>
2024-10-18 01:52:10 +0300

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,6 +1,6 @@
 const link = document.createElement('link');
 link.rel = 'stylesheet';
-link.href = 'https://6710fa175655dd0ef8a4e581--lighthearted-speculoos-01ce2f.netlify.app/style.css';
+link.href = 'https://lighthearted-speculoos-01ce2f.netlify.app/style.css';
 
 link.onload = () => {
   console.log('Stylesheet loaded successfully');
